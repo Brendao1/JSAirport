7b5eb9715b24efc286e7ff41be9179477eb8395f
matt <mwhitaker1006@gmail.com>
2019-03-05 15:37:45 +0000

    passed test for land plane, added failing test for take off plane

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -1,10 +1,17 @@
-describe('Airport', function(){
-  it('should land a plane')
-  var a = new Airport()
-  var p = new Plane()
-  a.land(p);
-  execpt(a).toInclude(p);
+describe('Airport', function() {
 
+  beforeEach(function() {
+    a = new Airport();
+    p = new Plane();
+  })
 
+  it('should land a plane', function() {
+    a.land(p);
+    expect(a.planes_landed).toContain(p);
+  })
 
+  it('should let a plane take off', function() {
+  a.takeOff(p)
+  expect(a.planes_landed).not.toContain(p)
+  })
 });
